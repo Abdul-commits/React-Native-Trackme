50f7b71ef21e5a07746152c8cdd28a290532dfa3
Abdul-commits <57894049+Abdul-commits@users.noreply.github.com>
2021-01-04 21:56:46 +0530

    Add files via upload

---
+++ Trackserver/src/index.js
@@ -0,0 +1,45 @@
+require('./models/User');
+require('./models/Track');
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const authRoutes = require('./routes/authRoutes');
+const trackRoutes = require('./routes/trackRoutes');
+const requireAuth = require('./middlewares/requireAuth');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(authRoutes);
+app.use(trackRoutes);
+
+const mongoUri = 'mongodb+srv://admin:passwordispassword@cluster0.ltops.mongodb.net/test?retryWrites=true&w=majority'
+mongoose.connect(mongoUri, {
+
+    useNewUrlParser: true,
+    useCreateIndex: true
+
+
+});
+
+mongoose.connection.on('connected', ()=> {
+    console.log('Connected to mongo instance');
+
+
+});
+
+
+mongoose.connection.on('connected', () => {
+  console.log('Connected to mongo instance');
+});
+mongoose.connection.on('error', err => {
+  console.error('Error connecting to mongo', err);
+});
+
+app.get('/', requireAuth, (req, res) => {
+  res.send(`Your email: ${req.user.email}`);
+});
+
+app.listen(3000, () => {
+  console.log('Listening on port 3000 Aye Aye');
+});
